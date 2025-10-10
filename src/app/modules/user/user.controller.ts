515d9f9a4399ad415ee328d3ec6180acf754385c
Abu Raihan <aburaihanrahmani@gmail.com>
2025-10-10 08:19:55 -0700

    Avoid Repetition of Try-Catch, use catchAsync

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,20 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from "express";
 import { User } from "./user.model";
+import catchAsync from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { name, email } = req.body;
-    const user = await User.create({ name, email });
-    res.status(200).json({
-      success: true,
-      message: "User registered successfully",
-      data: user,
-    });
-  } catch (error: any) {
-    next(error);
-  }
-};
+const createUser = catchAsync(async (req, res) => {
+  const { name, email } = req.body;
+  const user = await User.create({ name, email });
+  res.status(200).json({
+    success: true,
+    message: "User registered successfully",
+    data: user,
+  });
+});
 
 export const userController = {
   createUser,
