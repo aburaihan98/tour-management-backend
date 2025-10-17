bd54d059c27a4b17a76aafebdee4b64934afff2e
Abu Raihan <aburaihanrahmani@gmail.com>
2025-10-17 07:08:08 -0700

    Intro to JWT, create an AccessToken during Login

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,10 +3,10 @@ import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import httpStatus from "http-status-codes";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
   const { email, password } = payload;
-
   const emailExists = await User.findOne({ email });
   if (!emailExists) {
     throw new AppError(httpStatus.NOT_FOUND, "User does not exist");
@@ -16,12 +16,24 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
     password as string,
     emailExists.password as string
   );
+
   if (!isPasswordMatched) {
     throw new AppError(httpStatus.UNAUTHORIZED, "Password is incorrect");
   }
 
+  const userPayload = {
+    id: emailExists._id,
+    email: emailExists.email,
+    role: emailExists.role,
+  };
+
+  const token = jwt.sign(userPayload, "your_jwt_secret_key", {
+    expiresIn: "1d",
+  });
+
   return {
     email: emailExists.email,
+    token,
   };
 };
 
